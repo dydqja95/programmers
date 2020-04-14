7a8fd77125847ab8c3f0cdf666a54e02f26576fb
dydqja95 <49568167+dydqja95@users.noreply.github.com>
2020-04-14 14:22:40 +0900

    Update solution.java

--- 같은숫자는싫어/solution.java
+++ 같은숫자는싫어/solution.java
@@ -4,27 +4,30 @@ import java.util.*;
 
 public class solution {
 
-	public ArrayList <Integer> solution(int []arr) {
-		ArrayList <Integer> temp = new ArrayList<>();
-		int num;
-		for(int i = 1; i < arr.length; i++) {
-			
-			if(arr[i-1] != arr[i]) {
-				temp.add(arr[i-1]);
-			}
-			else {
-				i++;
-			}
-		}
-		return temp;
-	}
+	public static int[] solution(int[] array) {
+	       
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        
+        for(int i = 0; i < array.length-1; i++) {
+           if(array[i] != array[i+1]) {
+              arrayList.add(array[i]);
+           }
+        }
+        
+        arrayList.add(array[array.length-1]);
+        
+        int[] answer = new int[arrayList.size()];   
+        for(int i = 0; i < answer.length; i++) {
+           answer[i] = arrayList.get(i);
+        }        
+        return answer;
+     }
 	public static void main(String[] args) {
 		int[] arr1 = {1,1,3,3,0,1,1};
 		int[] arr2 = {4,4,4,3,3};
 			
 		solution sol = new solution();
 		System.out.println(sol.solution(arr1));
+		System.out.println(sol.solution(arr2));
 	}
 }
-
-
