62102b53eaea11ec73b1e5e4f3e45fddf1b51cda
dydqja95 <49568167+dydqja95@users.noreply.github.com>
2020-06-15 17:01:39 +0900

    Create solution.py

---
+++ level_2/큰수만들기/solution.py
@@ -0,0 +1,17 @@
+def solution(number, k):
+    collected = []
+    for i, num in enumerate(number):
+        while len(collected) > 0 and collected[-1] < num and k >0:
+            collected.pop()
+            k-=1
+        if k == 0:
+            collected += list(number[i:])
+            break
+        collected.append(num)
+    if k > 0 :
+        collected = collected[:-k] 
+    else:
+        collected
+        print(collected)
+    answer = ''.join(collected)
+    return answer
